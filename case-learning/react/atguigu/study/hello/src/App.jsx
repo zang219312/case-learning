ca09e32fb20608883b080d6605dc744b6dcbb739
zhangding <2324674973@qq.com>
2022-03-13 21:05:40 +0800

    add pubsUbJs

--- case-learning/react/atguigu/study/hello/src/App.jsx
+++ case-learning/react/atguigu/study/hello/src/App.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
-
+import Search from './components/Search'
+import List from './components/List'
+import './App.css'
 export default class App extends Component {
   render() {
-    return <div>app</div>
+    return (
+      <div className="container">
+        <Search />
+        <List />
+      </div>
+    )
   }
 }
