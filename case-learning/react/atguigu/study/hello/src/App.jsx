9d3068223ff28a235ccf06b4d362d9e74b6bc902
zhangding <2324674973@qq.com>
2022-03-15 21:06:01 +0800

    基本结构

--- case-learning/react/atguigu/study/hello/src/App.jsx
+++ case-learning/react/atguigu/study/hello/src/App.jsx
@@ -1,14 +1,6 @@
 import React, { Component } from 'react'
-import Search from './components/Search'
-import List from './components/List'
-import './App.css'
 export default class App extends Component {
   render() {
-    return (
-      <div className="container">
-        <Search />
-        <List />
-      </div>
-    )
+    return <div>app</div>
   }
 }
