61fec1afcd76e064512fe4efe640b7a461e4fd97
zhangding <2324674973@qq.com>
2022-03-20 13:20:40 +0800

    编程式路由导航

---
+++ case-learning/react/atguigu/study/hello/14_编程时路由导航/pages/Home/Message/Detail/index.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+// import qs from 'qs'
+const DetailData = [
+  {
+    id: '01',
+    content: '消息1'
+  },
+  {
+    id: '02',
+    content: '消息2'
+  },
+  {
+    id: '03',
+    content: '消息3'
+  }
+]
+export default class Detail extends Component {
+  render() {
+    // 接受params 参数
+    // const { id, title } = this.props.match.params
+
+    // 接受 search 参数
+    /* const { search } = this.props.location
+    const { id, title } = qs.parse(search.slice(1)) */
+
+    // 接受 state 参数
+    const { id, title } = this.props.location.state || {}
+    const res = DetailData.find(v => v.id === id) || {}
+    return (
+      <ul>
+        <li>id:{id}</li>
+        <li>title:{title}</li>
+        <li>content:{res.content}</li>
+      </ul>
+    )
+  }
+}
---
+++ case-learning/react/atguigu/study/hello/14_编程时路由导航/pages/Home/Message/index.jsx
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+import { Route, Switch, Redirect, Link } from 'react-router-dom'
+import Detail from './Detail'
+export default class Message extends Component {
+  state = {
+    list: [
+      {
+        id: '01',
+        title: 'message001'
+      },
+      {
+        id: '02',
+        title: 'message002'
+      },
+      {
+        id: '03',
+        title: 'message003'
+      }
+    ]
+  }
+
+  pushShow = (id, title) => {
+    // this.props.history.push(`/home/message/detail/${id}/${title}`)
+
+    this.props.history.push(`/home/message/detail`, { id, title })
+  }
+
+  replaceShow = (id, title) => {
+    // this.props.history.replace(`/home/message/detail/${id}/${title}`)
+
+    this.props.history.replace(`/home/message/detail`, { id, title })
+  }
+
+  back = () => {
+    this.props.history.goBack()
+  }
+  forward = () => {
+    this.props.history.goForward()
+  }
+
+  go = () => {
+    this.props.history.go(2)
+  }
+
+  render() {
+    return (
+      <div className="message">
+        <ul className="message-ul">
+          {this.state.list.map(v => {
+            return (
+              <li key={v.id}>
+                {/* 传递 params 参数 */}
+                <Link to={`/home/message/detail/${v.id}/${v.title}`}>
+                  {v.title}
+                </Link>
+
+                <button onClick={() => this.pushShow(v.id, v.title)}>
+                  push
+                </button>
+                <button onClick={() => this.replaceShow(v.id, v.title)}>
+                  replace
+                </button>
+
+                {/* 传递 search 参数 */}
+                {/*  <Link to={`/home/message/detail/?id=${v.id}&title=${v.title}`}>
+                  {v.title}
+                </Link> */}
+
+                {/* 传递state参数 */}
+                {/* <Link
+                  to={{
+                    pathname: '/home/message/detail',
+                    state: { id: v.id, title: v.title }
+                  }}
+                >
+                  {v.title}
+                </Link> */}
+              </li>
+            )
+          })}
+        </ul>
+        <Switch>
+          {/* 声明接受 params 参数 */}
+          {/* <Route
+            path="/home/message/detail/:id/:title"
+            component={Detail}
+          ></Route> */}
+          {/* search 参数无需声明接受  */}
+          {/* <Route path="/home/message/detail" component={Detail}></Route> */}
+          {/* state 参数无需声明接受  */}
+          <Route path="/home/message/detail" component={Detail}></Route>
+          {/* <Redirect
+            to={{
+              pathname: `/home/message/detail/${this.state.list[0].id}/${this.state.list[0].title}`
+            }}
+          ></Redirect> */}
+
+          <Redirect
+            to={{
+              pathname: '/home/message/detail',
+              state: {
+                id: this.state.list[0].id,
+                title: this.state.list[0].title
+              }
+            }}
+          ></Redirect>
+        </Switch>
+
+        <button onClick={() => this.forward()}>前进</button>
+        <button onClick={() => this.back()}>后退</button>
+        <button onClick={() => this.go()}>go</button>
+      </div>
+    )
+  }
+}
