ae346fc136c3c4795cd62b015b558dd0608706ab
zhangding <2324674973@qq.com>
2022-04-13 22:23:40 +0800

    init

---
+++ case-learning/websocket/vue2/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Login">
+    <input type="text" />
+    <span @click="handleEnterClick">登录</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  methods: {
+    handleEnterClick() {},
+  },
+};
+</script>
