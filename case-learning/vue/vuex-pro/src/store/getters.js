07c5247a650b4c5bee613f1ce2bcd1395556fbd0
zhangding <2324674973@qq.com>
2022-09-11 14:31:04 +0800

    fix:删除多余文件

--- case-learning/vue/vuex-pro/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-export default {
-  getMyInfo(state) {
-    console.log(state);
-    return `${state.name},${state.age}`;
-  },
-  setData(state, data) {
-    state.data = data;
-  },
-};
--- case-learning/vue/vuex-pro/src/store/tab/state.js
+++
@@ -1,3 +0,0 @@
-export default {
-  curIdx: 0,
-};
